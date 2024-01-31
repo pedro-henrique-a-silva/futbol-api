5e2ccfa79dd047e7f084fddf10cae04f4501e9b1
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-31 16:54:48 -0300

    Implementing UserController login method

---
+++ app/backend/src/controller/UsersController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import UsersService from '../services/UsersService';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
+
+export default class UsersController {
+  constructor(
+    private usersService: UsersService = new UsersService(),
+  ) {}
+
+  public async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const { status, data } = await this.usersService.login(email, password);
+
+    const code = mapStatusHTTP(status);
+
+    return res.status(code).json(data);
+  }
+}
