d2e2fd85e88ae21cd17b3b0e08bb89696cf0d280
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-31 17:29:13 -0300

    Add custom status to validation middleware

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -1,11 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import joiValidateLogin from '../utils/joi/login';
+import mapStatusHTTP from '../utils/mapStatusHTTP';
 
 export default class Validations {
   public static validateLogin(req: Request, res: Response, next: NextFunction) {
     const { error } = joiValidateLogin(req.body);
 
-    if (error) return res.status(400).json({ message: 'Invalid Data' });
+    if (error) {
+      const [status, message] = error.message.split('|');
+      return res.status(mapStatusHTTP(status)).json({ message });
+    }
 
     next();
   }
