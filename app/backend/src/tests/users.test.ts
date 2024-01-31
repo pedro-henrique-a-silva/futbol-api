1c3358f1e709732c9f3c3ecb1769b5513973e971
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-31 19:59:49 -0300

    Making tests for GET /login/role

--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -6,6 +6,8 @@ import chaiHttp = require('chai-http');
 import { App } from '../app';
 import SequelizeUsers from '../database/models/SequelizeUsers';
 import mocks from './mocks/users';
+import JWT from '../utils/JWT';
+import Validations from '../middlewares/Validations';
 
 chai.use(chaiHttp);
 
@@ -74,6 +76,50 @@ describe('Testando rotas de Users', () => {
     expect(status).to.equal(401);
     expect(body).to.have.property('message');
     expect(body.message).to.be.an('string');
+  });
+
+  it('POST /login/role com token valído retornar status 200 com um objeto contendo a role do user', async () => {
+    sinon.stub(SequelizeUsers, 'findOne').resolves(mocks.userFromDB as any);
+    sinon.stub(JWT, 'verify').resolves("token_valido");
+
+    const { email, password } = mocks.userLoginBody
+    
+    const { status, body } = await chai.request(app).post('/login/role')
+      .set('authorization', 'validToken')
+      .send({ email, password });
+    
+    expect(status).to.equal(200);
+    expect(body).to.have.property('role');
+    expect(body.message).to.be.an('string');
+  });
+
+  it('POST /login/role sem token retornar status 401 com a mensagem de erro correta', async () => {
+    sinon.stub(SequelizeUsers, 'findOne').resolves(mocks.userFromDB as any);
+
+    const { email, password } = mocks.userLoginBody
+    
+    const { status, body } = await chai.request(app).post('/login/role')
+      .send({ email, password });
+    
+    expect(status).to.equal(401);
+    expect(body).to.have.property('role');
+    expect(body.message).to.be.an('string');
+    expect(body.message).to.equal('Token not found');
+  });
+
+  it('POST /login/role com token inválido retornar status 401 com a mensagem de erro correta', async () => {
+    sinon.stub(SequelizeUsers, 'findOne').resolves(mocks.userFromDB as any);
+    sinon.stub(JWT, 'verify').resolves("Token must be a valid token");
+
+    const { email, password } = mocks.userLoginBody
+    
+    const { status, body } = await chai.request(app).post('/login/role')
+      .send({ email, password });
+    
+    expect(status).to.equal(200);
+    expect(body).to.have.property('role');
+    expect(body.message).to.be.an('string');
+    expect(body.message).to.equal('Token must be a valid token');
   });
 
 
