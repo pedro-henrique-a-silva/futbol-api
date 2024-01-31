bf104257852e21eaf6a65ba4f57fbd2a1781c6e9
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-31 17:30:46 -0300

    add custom messages to validate body

--- app/backend/src/utils/joi/login.ts
+++ app/backend/src/utils/joi/login.ts
@@ -2,8 +2,14 @@ import * as Joi from 'joi';
 
 const joiValidateLogin = (input: Record<string, string>) => {
   const schema = Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+    email: Joi.string().email().required().messages({
+      'any.required': 'INVALID_DATA|All fields must be filled',
+      'string.email': 'UNAUTHORIZED|Invalid email or password',
+    }),
+    password: Joi.string().min(6).required().messages({
+      'any.required': 'INVALID_DATA|All fields must be filled',
+      'string.min': 'UNAUTHORIZED|Invalid email or password',
+    }),
   });
   return schema.validate(input);
 };
