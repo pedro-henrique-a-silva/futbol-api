3f10ed2b3aaa703c8c8383e8161521a8b294a8b7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-31 19:58:20 -0300

    Implementing validateToken

--- app/backend/src/middlewares/Validations.ts
+++ app/backend/src/middlewares/Validations.ts
@@ -1,15 +1,45 @@
 import { Request, Response, NextFunction } from 'express';
+import JWT from '../utils/JWT';
 import joiValidateLogin from '../utils/joi/login';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
 
 export default class Validations {
-  public static validateLogin(req: Request, res: Response, next: NextFunction) {
+  private static invalidTokenMessage = 'Token must be a valid token';
+
+  static validateLogin(req: Request, res: Response, next: NextFunction): Response | void {
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
     const { error } = joiValidateLogin(req.body);
 
     if (error) {
       const [status, message] = error.message.split('|');
       return res.status(mapStatusHTTP(status)).json({ message });
     }
+
+    next();
+  }
+
+  static validateToken(req: Request, res: Response, next: NextFunction):
+  Response | void {
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+
+    // bearer token
+    const token = req.headers.authorization.split(' ')[1]; // [ "bearer", "token" ]
+
+    if (!token) {
+      return res.status(401).json({ message: Validations.invalidTokenMessage });
+    }
+
+    const validToken = JWT.verify(token);
+
+    if (validToken === 'Token must be a valid token') {
+      return res.status(401).json({ message: validToken });
+    }
+
+    res.locals.auth = validToken;
 
     next();
   }
