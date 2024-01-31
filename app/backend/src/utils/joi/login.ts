68ffebb7f40bc09291cc2e281e2bf6e764a9940a
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-31 19:59:23 -0300

    Fixing some status for response

--- app/backend/src/utils/joi/login.ts
+++ app/backend/src/utils/joi/login.ts
@@ -1,14 +1,17 @@
 import * as Joi from 'joi';
 
+const minResponse = 'UNAUTHORIZED|Invalid email or password';
+
 const joiValidateLogin = (input: Record<string, string>) => {
   const schema = Joi.object().keys({
-    email: Joi.string().email().required().messages({
-      'any.required': 'INVALID_DATA|All fields must be filled',
-      'string.email': 'UNAUTHORIZED|Invalid email or password',
-    }),
+    email: Joi.string().email().min(3).required()
+      .messages({
+        'any.required': 'INVALID_DATA|All fields must be filled',
+        'string.email': 'UNAUTHORIZED|Invalid email or password',
+      }),
     password: Joi.string().min(6).required().messages({
       'any.required': 'INVALID_DATA|All fields must be filled',
-      'string.min': 'UNAUTHORIZED|Invalid email or password',
+      'string.min': minResponse,
     }),
   });
   return schema.validate(input);
