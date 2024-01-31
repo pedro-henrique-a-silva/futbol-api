b2316d2c1ada0ffee25474949bddea0caa51d843
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-01-31 14:44:02 -0300

    Implementing route /:id

--- app/backend/src/router/teams.routes.ts
+++ app/backend/src/router/teams.routes.ts
@@ -6,5 +6,6 @@ const teamsController = new TeamsController();
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => teamsController.getAllTeams(req, res));
+router.get('/:id', (req: Request, res: Response) => teamsController.getById(req, res));
 
 export default router;
