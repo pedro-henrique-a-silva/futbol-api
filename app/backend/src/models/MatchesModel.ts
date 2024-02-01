6f2f53893c6390f27ed6214ef5f66be3864a24ef
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-01 17:53:59 -0300

    Implementing findAllInProgress

--- app/backend/src/models/MatchesModel.ts
+++ app/backend/src/models/MatchesModel.ts
@@ -5,9 +5,21 @@ import SequelizeTeams from '../database/models/SequelizeTeams';
 
 export default class MatchesModel implements IMatchesModel {
   private model = SequelizeMatches;
+  private booleanMap: Record<string, number> = { true: 1, false: 0 };
 
   public async findAll(): Promise<IMatch[]> {
     const matches = await this.model.findAll({
+      include: [
+        { model: SequelizeTeams, attributes: ['teamName'], as: 'homeTeam' },
+        { model: SequelizeTeams, attributes: ['teamName'], as: 'awayTeam' },
+      ],
+    });
+    return matches.map((match) => match.dataValues);
+  }
+
+  public async findAllInProgress(inProgress: string): Promise<IMatch[]> {
+    const matches = await this.model.findAll({
+      where: { inProgress: this.booleanMap[inProgress] },
       include: [
         { model: SequelizeTeams, attributes: ['teamName'], as: 'homeTeam' },
         { model: SequelizeTeams, attributes: ['teamName'], as: 'awayTeam' },
