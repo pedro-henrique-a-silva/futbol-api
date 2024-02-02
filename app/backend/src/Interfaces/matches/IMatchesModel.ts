f8b60381e0e8a4e799e837a02fb36b7443c63264
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-02 10:53:10 -0300

    add updateMatch method to IMatchesModel interface

--- app/backend/src/Interfaces/matches/IMatchesModel.ts
+++ app/backend/src/Interfaces/matches/IMatchesModel.ts
@@ -4,4 +4,5 @@ import { IMatch } from './IMatches';
 export type IMatchesModel = Omit<ICRUDModelReader<IMatch>, 'findById'> & {
   findAllInProgress(inProgress: string): Promise<IMatch[]>;
   finishMatch(id: string): Promise<void>;
+  updateMatch(id: string, match: { homeTeamGoals: number; awayTeamGoals: number }): Promise<void>;
 };
