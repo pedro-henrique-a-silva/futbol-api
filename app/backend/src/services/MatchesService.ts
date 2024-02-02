6515ff757e168dc69d437ab970d38e6e59067b77
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-02 12:04:05 -0300

    Implement insertNewMatches method

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -2,10 +2,13 @@ import { IMatchesModel } from '../Interfaces/matches/IMatchesModel';
 import MatchesModel from '../models/MatchesModel';
 import { IMatch } from '../Interfaces/matches/IMatches';
 import { ServiceResponse } from '../Interfaces/ServiceResponse';
+import { ITeamsModel } from '../Interfaces/teams/ITeamsModel';
+import TeamsModel from '../models/TeamsModel';
 
 export default class MatchesService {
   constructor(
     private matchesModel: IMatchesModel = new MatchesModel(),
+    private teamsModel: ITeamsModel = new TeamsModel(),
   ) {}
 
   public async getAllMatches(inProgress: string | null): Promise<ServiceResponse<IMatch[]>> {
@@ -32,5 +35,24 @@ export default class MatchesService {
     await this.matchesModel.updateMatch(id, match);
 
     return { status: 'SUCCESSFUL', data: { message: 'Updated' } };
+  }
+
+  public async insertNewMatch(match: IMatch): Promise<ServiceResponse<IMatch>> {
+    const homeTeam = await this.teamsModel.findById(match.homeTeamId);
+    const awayTeam = await this.teamsModel.findById(match.awayTeamId);
+
+    if (!homeTeam || !awayTeam) {
+      return { status: 'NOT_FOUND', data: { message: 'There is no team with such id!' } };
+    }
+
+    if (homeTeam.teamName === awayTeam.teamName) {
+      return {
+        status: 'UNABLE_TO_PROCESS',
+        data: { message: 'It is not possible to create a match with two equal teams' } };
+    }
+
+    const insertd = await this.matchesModel.create(match);
+
+    return { status: 'CREATED', data: insertd };
   }
 }
