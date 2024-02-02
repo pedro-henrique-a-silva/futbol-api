460723eafa86d28d4b43bdc1e21fdc3f25641b7b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-02 11:21:04 -0300

    add mocks

--- app/backend/src/tests/mocks/matches.ts
+++ app/backend/src/tests/mocks/matches.ts
@@ -29,6 +29,78 @@ const allMatchesFromDB = [
   }
 ]
 
+const allMatchesInProgressFromDB = [
+  {
+    dataValues: {
+      id: 41,
+      homeTeamId: 16,
+      homeTeamGoals: 2,
+      awayTeamId: 9,
+      awayTeamGoals: 0,
+      inProgress: true,
+      homeTeam: {
+        teamName: "São Paulo"
+      },
+      awayTeam: {
+        teamName: "Internacional"
+      }
+    }
+  },
+  {
+    dataValues: {
+      id: 42,
+      homeTeamId: 6,
+      homeTeamGoals: 1,
+      awayTeamId: 1,
+      awayTeamGoals: 0,
+      inProgress: true,
+      homeTeam: {
+        teamName: "Ferroviária"
+      },
+      awayTeam: {
+        teamName: "Avaí/Kindermann"
+      }
+    }
+  }
+]
+
+const allMatchesNotInProgressFromDB = [
+  {
+    dataValues: {
+      id: 1,
+      homeTeamId: 16,
+      homeTeamGoals: 1,
+      awayTeamId: 8,
+      awayTeamGoals: 1,
+      inProgress: false,
+      homeTeam: {
+        teamName: "São Paulo"
+      },
+      awayTeam: {
+        teamName: "Grêmio"
+      }
+    }
+  },
+  {
+    dataValues: {
+      id: 2,
+      homeTeamId: 9,
+      homeTeamGoals: 1,
+      awayTeamId: 14,
+      awayTeamGoals: 1,
+      inProgress: false,
+      homeTeam: {
+        teamName: "Internacional"
+      },
+      awayTeam: {
+        teamName: "Santos"
+      }
+    }
+  }
+]
+
 export default {
   allMatchesFromDB,
+  allMatchesInProgressFromDB,
+  allMatchesNotInProgressFromDB
 };
