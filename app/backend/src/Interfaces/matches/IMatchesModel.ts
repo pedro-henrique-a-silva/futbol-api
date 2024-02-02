7c7eeb0237e4a998efe60737420fd426498c5626
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-02 12:02:51 -0300

    Update IMatchesModel interface

--- app/backend/src/Interfaces/matches/IMatchesModel.ts
+++ app/backend/src/Interfaces/matches/IMatchesModel.ts
@@ -1,8 +1,11 @@
-import { ICRUDModelReader } from '../ICRUDModel';
+import { ICRUDModelCreator, ICRUDModelReader } from '../ICRUDModel';
 import { IMatch } from './IMatches';
 
-export type IMatchesModel = Omit<ICRUDModelReader<IMatch>, 'findById'> & {
-  findAllInProgress(inProgress: string): Promise<IMatch[]>;
-  finishMatch(id: string): Promise<void>;
-  updateMatch(id: string, match: { homeTeamGoals: number; awayTeamGoals: number }): Promise<void>;
-};
+export type IMatchesModel =
+  ICRUDModelReader<IMatch>
+  & ICRUDModelCreator<IMatch>
+  & {
+    findAllInProgress(inProgress: string): Promise<IMatch[]>;
+    finishMatch(id: string): Promise<void>;
+    updateMatch(id: string, match: { homeTeamGoals: number; awayTeamGoals: number }): Promise<void>;
+  };
