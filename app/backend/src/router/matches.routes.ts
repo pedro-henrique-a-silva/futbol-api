16c92cf93522144b273d9397da7dbbc0ccbe35ab
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-02 10:13:27 -0300

    Implement finish route

--- app/backend/src/router/matches.routes.ts
+++ app/backend/src/router/matches.routes.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express';
+import Validations from '../middlewares/Validations';
 import MatchesController from '../controller/MatchesController';
 
 const matchesController = new MatchesController();
@@ -8,6 +9,11 @@ const router = Router();
 router.get(
   '/',
   (req: Request, res: Response) => matchesController.getAllMatches(req, res),
+);
+router.patch(
+  '/:id/finish',
+  Validations.validateToken,
+  (req: Request, res: Response) => matchesController.finishMatch(req, res),
 );
 
 export default router;
