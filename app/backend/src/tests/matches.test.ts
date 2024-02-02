9c25b171539f38972a24e9d40a502d651b93792d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-02 11:21:13 -0300

    testing new routes

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -6,6 +6,7 @@ import chaiHttp = require('chai-http');
 import { App } from '../app';
 import SequelizeMatches from '../database/models/SequelizeMatches';
 import mocks from './mocks/matches';
+import JWT from '../utils/JWT';
 
 chai.use(chaiHttp);
 
@@ -21,7 +22,48 @@ describe('Testando rota de Matches', () => {
     const { status, body } = await chai.request(app).get('/matches');
 
     expect(status).to.equal(200);
-    expect(body).to.be.an('array');;
+    expect(body).to.be.an('array');
+  });
+
+  it('GET /matches?inProgress=true Deve retornar uma lista de patidas em andamento com status 200', async () => {
+    sinon.stub(SequelizeMatches, 'findAll').resolves(mocks.allMatchesInProgressFromDB as any);
+
+    const { status, body } = await chai.request(app).get('/matches?inProgress=true');
+
+    expect(status).to.equal(200);
+    expect(body).to.be.an('array');
+    expect(body).to.deep.equal(mocks.allMatchesInProgressFromDB.map((match) => match.dataValues));
+  });
+
+  it('GET /matches?inProgress=false Deve retornar uma lista de patidas que não estão em andamento com status 200', async () => {
+    sinon.stub(SequelizeMatches, 'findAll').resolves(mocks.allMatchesNotInProgressFromDB as any);
+
+    const { status, body } = await chai.request(app).get('/matches?inProgress=false');
+
+    expect(status).to.equal(200);
+    expect(body).to.be.an('array');
+    expect(body).to.deep.equal(mocks.allMatchesNotInProgressFromDB.map((match) => match.dataValues));
+  });
+
+  it('patch /matches/:id/finish deve atualizar o resultado de uma partida e retornar status 200', async () => {
+    sinon.stub(SequelizeMatches, 'update').resolves();
+    sinon.stub(JWT, 'verify').returns('valideToken');
+
+    const { status, body } = await chai.request(app).patch('/matches/1/finish')
+      .set('authorization', 'Bearer validToken');
+
+    expect(status).to.equal(200);
+  });
+
+  it('patch /matches/:id deve atualizar o placar de uma partida e retornar status 200', async () => {
+    sinon.stub(SequelizeMatches, 'update').resolves();
+    sinon.stub(JWT, 'verify').returns('valideToken');
+
+    const { status, body } = await chai.request(app).patch('/matches/1')
+      .set('authorization', 'Bearer validToken')
+      .send({ homeTeamGoals: 2, awayTeamGoals: 2 });
+
+    expect(status).to.equal(200);
   });
 
 
