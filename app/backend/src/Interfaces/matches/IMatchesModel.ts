b9df2186b3e3349ce4dbbd3d03fd62a3824f71dd
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-02 10:12:57 -0300

    Add finishMatch method to IMatchesModel interface

--- app/backend/src/Interfaces/matches/IMatchesModel.ts
+++ app/backend/src/Interfaces/matches/IMatchesModel.ts
@@ -3,4 +3,5 @@ import { IMatch } from './IMatches';
 
 export type IMatchesModel = Omit<ICRUDModelReader<IMatch>, 'findById'> & {
   findAllInProgress(inProgress: string): Promise<IMatch[]>;
+  finishMatch(id: string): Promise<void>;
 };
