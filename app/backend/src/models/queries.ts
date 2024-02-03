8abd4a8f4890da4976cef61284ba5f524aaf5385
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-03 14:39:04 -0300

    fixing queries to work with away teams

--- app/backend/src/models/queries.ts
+++ app/backend/src/models/queries.ts
@@ -1,7 +1,7 @@
 export const subQuerieTotalVictories = (homeOrAway: string, otherTeam: string) => (
   `(SELECT COUNT(*) FROM matches  
           WHERE matches.${homeOrAway}_team_goals > matches.${otherTeam}_team_goals AND
-          ${homeOrAway}Team.id = matches.home_team_id AND
+          ${homeOrAway}Team.id = matches.${homeOrAway}_team_id AND
           matches.in_progress = 0
   )`
 );
@@ -9,7 +9,7 @@ export const subQuerieTotalVictories = (homeOrAway: string, otherTeam: string) =
 export const subQuerieTotalLosses = (homeOrAway: string, otherTeam: string) => (
   `(SELECT COUNT(*) FROM matches  
           WHERE matches.${homeOrAway}_team_goals < matches.${otherTeam}_team_goals AND
-          ${homeOrAway}Team.id = matches.home_team_id AND
+          ${homeOrAway}Team.id = matches.${homeOrAway}_team_id AND
           matches.in_progress = 0
   )`
 );
@@ -17,7 +17,7 @@ export const subQuerieTotalLosses = (homeOrAway: string, otherTeam: string) => (
 export const subQuerieTotalDraws = (homeOrAway: string, otherTeam: string) => (
   `(SELECT COUNT(*) FROM matches  
           WHERE matches.${homeOrAway}_team_goals = matches.${otherTeam}_team_goals AND
-          ${homeOrAway}Team.id = matches.home_team_id AND
+          ${homeOrAway}Team.id = matches.${homeOrAway}_team_id AND
           matches.in_progress = 0
           
   )`
