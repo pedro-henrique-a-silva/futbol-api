91d1f7061b9a5673a2ad5b7872d9d07addd060bc
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-03 12:58:56 -0300

    Add new methods to ITeamModel interface

--- app/backend/src/Interfaces/teams/ITeamsModel.ts
+++ app/backend/src/Interfaces/teams/ITeamsModel.ts
@@ -1,4 +1,8 @@
 import { ICRUDModelReader } from '../ICRUDModel';
+import { IGamesStatistics, IGoalsStatistics } from '../leaderBoard/ILeaderBoard';
 import { ITeams } from './ITeams';
 
-export type ITeamsModel = ICRUDModelReader<ITeams>;
+export type ITeamsModel = ICRUDModelReader<ITeams> & {
+  getGolsStatistics(homeOrAway: string): Promise<IGoalsStatistics[]>;
+  getGamesStatistics(homeOrAway: string): Promise<IGamesStatistics[]>;
+};
