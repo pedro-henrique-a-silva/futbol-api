9dd6a674b1f4f0243545213a3cb7dcb5069817d8
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-03 12:59:14 -0300

    Creating custom queries

---
+++ app/backend/src/models/queries.ts
@@ -0,0 +1,24 @@
+export const subQuerieTotalVictories = (homeOrAway: string, otherTeam: string) => (
+  `(SELECT COUNT(*) FROM matches  
+          WHERE matches.${homeOrAway}_team_goals > matches.${otherTeam}_team_goals AND
+          ${homeOrAway}Team.id = matches.home_team_id AND
+          matches.in_progress = 0
+  )`
+);
+
+export const subQuerieTotalLosses = (homeOrAway: string, otherTeam: string) => (
+  `(SELECT COUNT(*) FROM matches  
+          WHERE matches.${homeOrAway}_team_goals < matches.${otherTeam}_team_goals AND
+          ${homeOrAway}Team.id = matches.home_team_id AND
+          matches.in_progress = 0
+  )`
+);
+
+export const subQuerieTotalDraws = (homeOrAway: string, otherTeam: string) => (
+  `(SELECT COUNT(*) FROM matches  
+          WHERE matches.${homeOrAway}_team_goals = matches.${otherTeam}_team_goals AND
+          ${homeOrAway}Team.id = matches.home_team_id AND
+          matches.in_progress = 0
+          
+  )`
+);
