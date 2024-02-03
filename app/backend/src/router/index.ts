720d91dfb42f621b86c1cfcb7e208e16d5868da6
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-03 13:00:10 -0300

    Add route /leaderboard to barrel

--- app/backend/src/router/index.ts
+++ app/backend/src/router/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import teamsRouter from './teams.routes';
 import usersRouter from './users.routes';
 import matchesRouter from './matches.routes';
+import leaderRouter from './leaderBoard.routes';
 
 const router = Router();
 
 router.use('/teams', teamsRouter);
 router.use('/login', usersRouter);
 router.use('/matches', matchesRouter);
+router.use('/leaderBoard', leaderRouter);
 
 export default router;
